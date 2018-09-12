91a53cb5e85e825ba2d2c1e37814b07792c19cda
sk0rn <hyperthrone@gmail.com>
2018-09-12 21:10:28 +1100

    init commit

--- src/Demo.java
+++ src/Demo.java
@@ -7,19 +7,17 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        Double[] data = {12.6674, 34.0, 44.5, 1.00056, 3.14};
-
 
         Integer[] ints = {345, 78, 858, 45, 22, 90};
-        MathBox mb = new MathBox(data);
+        MathBox mb = new MathBox(ints);
 
 
         mb.dump();
         System.out.println(mb.summator());
         System.out.println(Arrays.toString(mb.splitter(2.0)));
-//        mb.addObject(4);
-//        mb.removeObject(45);
-//        mb.dump();
+        mb.addObject(4);
+        mb.removeObject(45);
+        mb.dump();
 
 
     }
